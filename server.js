398d8cea567b173cd10484c5e368f75c11d0977a
pammitu <pamstankova@gmail.com>
2023-06-07 14:17:52 -0700

    adding routes folder

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URI, {useNewURLParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DB_URI, {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+})
+
 .then(() => console.log('Database connection successful'))
 .catch((err) => console.log(`Database connection error:${err}`));
