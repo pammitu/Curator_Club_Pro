0eb76c77009fe9f3b292a4f80a1f864e5010a5ac
pammitu <pamstankova@gmail.com>
2023-06-07 14:39:57 -0700

    fetching user collection artworks

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,19 @@ const router = express.Router();
 const User = require('../models/user');
 
 router.post('/register', (req, res) => {
-    
-})
+    //register new user
+    //using the user model to create a new user in the datebase
+    //need to hash the users password before saving it
+});
+
+router.get('/:username/collection', function(req, res)  {
+    User
+    .findOne({ username: req.params.username })
+    .populate('collection')
+    .exec(function (err,user) {
+        if (err) return res.status(500).send(err);
+        res.send(user.collection);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+const userRoutes = require('.routes/userRoutes');
+const artworkRoutes = require('./routes/artworkRoutes');
+
+app.use('/user', userRoutes):
+app.use('/artworks', artworkRoutes);
+
 
 mongoose.connect(process.env.DB_URI, {
     useNewURLParser: true,
