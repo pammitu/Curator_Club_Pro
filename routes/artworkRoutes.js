54d1dbbcbf6dc5fbb067c6d3ca2dd80a012f271c
pammitu <pamstankova@gmail.com>
2023-06-07 15:14:46 -0700

    adding route for user collection

--- routes/artworkRoutes.js
+++ routes/artworkRoutes.js
@@ -2,5 +2,21 @@ const express = require('express');
 const router = express.router();
 const Artwork = require('../models/artwork');
 
+router.put('/:username/collection/add' (req, res) => {
+    const artworkId = req.body.artworkId;
+    user.findOneAndUpdate(
+        { username: req.params.username },
+        { $push: { collection: artworkId } },
+        function(err, result) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
+});
+
+
 
 module.exports = router;
