2fad1501064f44de5e191ade8a896b9c49338714
pammitu <pamstankova@gmail.com>
2023-06-09 17:01:12 -0700

    importing components

---
+++ frontend/src/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import SignIn from './components/SignIn';
+import NewGalleryForm from './components/NewGalleryForm';
+import FindArtist from './components/FindArtist';
+import FindArtwork from './components/FindArtwork';
+import Favorites from './components/Favorites';
+import Library from './components/Library';
+
