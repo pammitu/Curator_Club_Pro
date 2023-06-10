f30c4aff5de7a15d04084e80b1f4154c7548f417
pammitu <pamstankova@gmail.com>
2023-06-09 17:16:58 -0700

    creating layout folder for consistency

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -8,3 +8,20 @@ import FindArtwork from './components/FindArtwork';
 import Favorites from './components/Favorites';
 import Library from './components/Library';
 
+function App () {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/signin" component={SignIn}/>
+                <Route path="/new-gallery" component={NewGalleryForm}/>
+                <Route path="/find-artist" component={FindArtist}/>
+                <Route path="/find-artwork" component={FindArtwork}/>
+                <Route path="/favorites" component={Favorites}/>
+                <Route path="/library" component={Library}/>
+                <Route path="/" component={HomePage}/>
+            </Switch>
+        </Router>
+    )
+}
+
+export default App;
