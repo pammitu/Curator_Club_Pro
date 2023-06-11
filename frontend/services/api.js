bb23ac9ac28da1325bb8a412479b781073e2af33
pammitu <pamstankova@gmail.com>
2023-06-11 10:12:10 -0700

    drafting spi file to connect back anf front

---
+++ frontend/services/api.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000'; 
+
+export async function registerUser(userData) {
+    const response = await axios.post(`${API_URL}/user/register`, userData);
+    return response.data;
+}
+
+export async function loginUser(userData) {
+    const response = await axios.post(`${API_URL}/user/login`, userData);
+    return response.data;
+}
+
+export async function getArtworkCollection(username) {
+    const response = await axios.get(`${API_URL}/user/${username}/collection`);
+    return response.data;
+}
+
+export async function getFavorites(username) {
+    const response = await axios.get(`${API_URL}/user/${username}/favorites`);
+    return response.data;
+}
+
+export async function searchArtworkMet(query){
+    const response = await axios.get(`${API_URL}/artworks/search/met`, {
+        params: {
+            q: query
+        }
+    });
+    return response.data;
+}
+
+export async function searchArtworkEuropeana(query){
+    const response = await axios.get(`${API_URL}/artworks/search/europeana`, {
+        params: {
+            q: query
+        }
+    });
+    return response.data;
+}
+
+export async function addToCollection(username, artworkId) {
+    const response = await axios.put(`${API_URL}/artworks/${username}/collection/add`, { artworkId: artworkId });
+    return response.data;
+}
