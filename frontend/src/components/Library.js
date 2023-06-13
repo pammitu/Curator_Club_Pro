dacb4ebc010d00ae7e7e9b570c6b2eb30e0e57d3
pammitu <pamstankova@gmail.com>
2023-06-13 16:45:31 -0700

    im stupid

--- frontend/src/components/Library.js
+++ frontend/src/components/Library.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import Gallery from './Gallery';
 
 function Library() {
     const [galleries, setGalleries] = useState([]);
