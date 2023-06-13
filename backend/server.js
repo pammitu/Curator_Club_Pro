eefb75457fc271b9799c4b461b5cb00b2146990f
pammitu <pamstankova@gmail.com>
2023-06-13 10:57:14 -0700

    deployment part 3

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,15 @@ app.use('/api/user', userRoutes);
 app.use('/api/artworks', artworkRoutes);
 
 
-app.use(express.static(path.join(__dirname, '../../frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+app.get('/', (req, res) => {
+    res.send("Hello, World!");
+  });
+  
+  
 app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 mongoose.connect(process.env.DATABASE_URL, {
@@ -28,11 +33,6 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 .then(() => console.log('Database connection successful'))
 .catch((err) => console.log(`Database connection error:${err}`));
-
-app.get('/', (req, res) => {
-    res.send("Hello, World!");
-  });
-  
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server running on port 5000");
