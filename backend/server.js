9e4ace19754a75beb5974ce1537dce22713e5d1d
pammitu <pamstankova@gmail.com>
2023-06-13 11:01:19 -0700

    alot of confusion

--- backend/server.js
+++ backend/server.js
@@ -15,16 +15,16 @@ app.use('/api/user', userRoutes);
 app.use('/api/artworks', artworkRoutes);
 
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('/', (req, res) => {
     res.send("Hello, World!");
   });
   
-  
-app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-});
+
+// app.use('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+// });
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
