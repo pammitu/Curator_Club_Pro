cb677b4cfa81036e3112a709755c9006baeb4be3
pammitu <pamstankova@gmail.com>
2023-06-13 15:56:00 -0700

    trying a different route

--- frontend/src/components/ArtworkSearch.js
+++ frontend/src/components/ArtworkSearch.js
@@ -1,30 +1,30 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './ArtworkSearch.css';
+import { searchArtworkMet } from '../../services/api';
 
 function ArtworkSearch() {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [error, setError] = useState('')
 
-    
 
   const search = async (event) => {
     event.preventDefault();
     try {
-        const response = await axios.get(`https:/curator-club-pro.herokuapp.com/api/artworks/search/met?q=${searchQuery}`);
-        const searchData = response.data;
+        // const response = await axios.get(`https://curator-club-pro.herokuapp.com/api/artworks/search/met?q=${searchQuery}`);
+        // const searchData = response.data;
+        const searchData = await searchArtworkMet(searchQuery);
         console.log(searchData); 
 
         const limitedIDs = searchData.objectIDs.slice(0, 60);
 
-        // Use the Promise.all method to fetch all object details concurrently
+       
         const details = await Promise.all(limitedIDs.map(async id => {
           const detailResponse = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`);
             return detailResponse.data;
         }));
 
-        // Now details is an array of object data, so you can set it as the search results
         setSearchResults(details);
     } catch (error) {
         setError(error.message);
