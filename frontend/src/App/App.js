fb9bf15044cc33fb8b6e8c81e76325f16e059086
pammitu <pamstankova@gmail.com>
2023-06-13 17:52:14 -0700

    still trying to deploy

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -2,29 +2,20 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Layout from '../components/Layout';
 import MainPage from '../components/MainPage';
-import SignIn from '../components/SignIn';
 import NewGalleryForm from '../components/NewGalleryForm';
 import ArtworkSearch from '../components/ArtworkSearch';
 import Library from '../components/Library';
 
 function App () {
-    const [isSignedIn, setIsSignedIn] = useState(true);
     return (
         <Router>
             <Layout>
                 <Routes>
-                {isSignedIn ? (
-            <>
-                    <Route path="/signin" element={<SignIn/>}/>
-                    <Route path="/mainpage" element={<MainPage />} />
+                    <Route path="/" element={<MainPage />} />
                     <Route path="/new-gallery" element={<NewGalleryForm/>}/>
                     <Route path="/artwork-search" element={<ArtworkSearch/>}/>
                     <Route path="/library" element={<Library/>}/>
-                    <Route path="*" element={<Navigate to="/mainpage" />} />
-                    </>
-                ) : ( 
-                    <Route path="/" element={<Navigate to="/signin"  />} />
-                )}
+                    <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
             </Layout>
         </Router>
